efaf5906ef5371ac7b8bc705e192389b68f2a494
Ryan Barclay <mrryanbarclay@gmail.com>
2021-05-30 15:46:00 -0700

    here we go electric bugaloo

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import discord
 from discord.ext import commands
 
-TOKEN = open(“token.txt”,”r”).readline()
-client = commands.Bot(command_prefix = ‘--’)
+TOKEN = open("token.txt", "r")
+client = commands.Bot(command_prefix="--")
 
 # PUT CODE TO RUN BOT HERE
 
-@client.command()
-#We delete default help command
-client.remove_command('help')
+# We delete default help command
+client.remove_command("help")
 
 
-#answers with the ms latency
+# answers with the ms latency
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round (client.latency * 1000)}ms ')
+    await ctx.send(f"Pong! {round (client.latency * 1000)}ms ")
+
 
 # END OF BOT CODE
 print("CODE IS RUNNING")
